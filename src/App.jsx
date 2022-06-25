c53cce6bbb85bc7a18e3675944b6629d9c5e90c9
Wallace Júnior <wflj1997@hotmail.com>
2022-06-24 23:41:57 -0300

    Estilização da listagem

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
-import { Fragment } from "react";
-import { Counter } from "./components/Counter";
 import { RepositoryList } from "./components/RepositoryList";
 import "./styles/global.scss";
 
 export function App() {
-  return (
-    <Fragment>
-      <RepositoryList />
-      <Counter />
-    </Fragment>
-  );
+  return <RepositoryList />;
 }
