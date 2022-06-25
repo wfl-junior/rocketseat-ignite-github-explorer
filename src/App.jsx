1d82cba4de306287377549e90d97d4f770f6c2d5
Wallace Júnior <wflj1997@hotmail.com>
2022-06-24 23:23:42 -0300

    Estado do componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { Fragment } from "react";
+import { Counter } from "./components/Counter";
 import { RepositoryList } from "./components/RepositoryList";
 import "./styles/global.scss";
 
 export function App() {
-  return <RepositoryList />;
+  return (
+    <Fragment>
+      <RepositoryList />
+      <Counter />
+    </Fragment>
+  );
 }
