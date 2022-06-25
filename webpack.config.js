c693e6b1222c83dc2e10f9ef28e99cfa939adada
Wallace Júnior <wflj1997@hotmail.com>
2022-06-24 22:10:30 -0300

    Importando arquivos CSS

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,11 @@ module.exports = {
         exclude: /node_modules/,
         use: "babel-loader",
       },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
 };
